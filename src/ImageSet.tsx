6b37b0209a759af0d7b246df3ccd8aa2979dc10f
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2020-07-19 23:09:07 +0200

    Add missing key in set

--- src/ImageSet.tsx
+++ src/ImageSet.tsx
@@ -3,17 +3,15 @@ import urlBuilder, { CaravaggioOptions } from './urlBuilder';
 import useCaravaggio from './useCaravaggio';
 
 interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-  sets: Array<
-    {
-      type?: string;
-      rules: { [rule: string]: { opt: CaravaggioOptions; url?: string } };
-    }
-  >;
+  sets: Array<{
+    type?: string;
+    rules: { [rule: string]: { opt: CaravaggioOptions; url?: string } };
+  }>;
 }
 
 const ImageSet: React.SFC<ImageProps> = ({ sets, ...otherProps }) => {
   const caravaggioUrl = useCaravaggio();
-  const sources = sets.map((set) => {
+  const sources = sets.map((set, i) => {
     const srcSet = Object.entries(set.rules)
       .map(([rule, { opt, url }]) => {
         return `${urlBuilder(
@@ -23,7 +21,13 @@ const ImageSet: React.SFC<ImageProps> = ({ sets, ...otherProps }) => {
         )} ${rule}`;
       })
       .join(',');
-    return <source type={set.type} srcSet={srcSet} />;
+    return (
+      <source
+        type={set.type}
+        srcSet={srcSet}
+        key={`${otherProps.src}-set-${i}`}
+      />
+    );
   });
   return (
     <picture>
