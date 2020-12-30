0c0c8e18ec98f36db0c60347a818fb28b68a205a
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2020-12-30 18:00:53 +0100

    Accept ref in image

--- src/Image.tsx
+++ src/Image.tsx
@@ -6,7 +6,11 @@ export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   opt?: CaravaggioOptions;
 }
 
-export const Image: React.FC<ImageProps> = ({ opt, src, ...otherProps }) => {
-  const url = useCaravaggioImage(src as string, opt);
-  return <img src={url} {...otherProps} />;
-};
+export const Image = React.forwardRef<HTMLImageElement, ImageProps>(
+  ({ opt, src, ...otherProps }, ref) => {
+    const url = useCaravaggioImage(src as string, opt);
+    return <img src={url} {...otherProps} ref={ref} />;
+  },
+);
+
+Image.displayName = 'Image';
