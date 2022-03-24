6a7408c3ac1d77bbe04955702abe3819d7665500
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2022-03-24 11:40:40 +0100

    Update documentation

--- src/CaravaggioProvider.tsx
+++ src/CaravaggioProvider.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 export interface CaravaggioProviderProps {
+  /**
+   * Url for Caravaggio api. It can be absolute or relative
+   * @example
+   * Absolute url
+   * url="https://caravaggio.vercel.app"
+   *
+   * Relative url (e.g. on Next.js)
+   * url="/api/assets"
+   */
   url: string;
+  /**
+   * Caravaggio can only transform images with absolute urls. If you want to use relative urls,
+   * set this value and all the realtive image urls will be considered relative to this baseUrl.
+   * Optional, default: undefined
+   */
   baseUrl?: string;
 }
 
@@ -14,7 +28,7 @@ export const CaravaggioContext = React.createContext<CaravaggioContext | null>(
   null,
 );
 
-export const CaravaggioProvider: React.SFC<CaravaggioProviderProps> = ({
+export const CaravaggioProvider: React.FC<CaravaggioProviderProps> = ({
   children,
   url,
   baseUrl,
